67ebc5a717c5d5575259fa9664ede3cbc6f777f0
tadeuasarro <tadeutas@gmail.com>
2020-12-03 14:54:05 -0300

    Remove empty lines

--- src/index.js
+++ src/index.js
@@ -16,25 +16,17 @@ const myFunction = () => {
       .then((value) => {
         const mainContainer = document.getElementById('main-container');
         mainContainer.innerHTML = '';
-
         mainContainer.appendChild(weatherContainer(value));
-
         return true;
-
       })
       .catch(() => {
         const mainContainer = document.getElementById('main-container');
-
         const errorMessage = document.createElement('p');
         errorMessage.classList.add('alert', 'alert-danger', 'h3', 'text-center');
         errorMessage.innerHTML = 'Invalid city, please try again!';
-
         mainContainer.appendChild(errorMessage);
       });
-
   });
-
 };
-
 
 myFunction();
